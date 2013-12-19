194de0cb98d938a48abff68543d40433d7c640b3
Jeffrey Lee <leilitop@gmail.com>
2013-12-19 16:14:37 +0800

    线程相关改动

--- src/main/java/me/lilei/tfs/uploadfile/TFSResourcesFactory.java
+++ src/main/java/me/lilei/tfs/uploadfile/TFSResourcesFactory.java
@@ -2,6 +2,7 @@ package me.lilei.tfs.uploadfile;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,9 +14,10 @@ public class TFSResourcesFactory {
 	private static final Logger logger = LoggerFactory
 			.getLogger(TFSResourcesFactory.class);
 	
+	//线程启动阈值
 	private static int iThreadCount = 4;
 	
-	public static void blanceTFSResources(ConfigResources config,List<String> pathList){
+	public static void blanceTFSResourcesScheduler(ConfigResources config,List<String> pathList){
 		
 		int size = 0;
 		
@@ -24,24 +26,51 @@ public class TFSResourcesFactory {
 		}else{
 			
 			int diffCount = size / iThreadCount;
-			
-			ExecutorService exec = Executors.newCachedThreadPool();
-			for(int i = 0; i <iThreadCount;i++){
+			final CountDownLatch latch = new CountDownLatch(iThreadCount);
+			final ExecutorService exec = Executors.newCachedThreadPool();
+			final TFSResources tfsrHook;
+			for(int i = 0; i < iThreadCount; i++ ){
 				TFSResources tfsr = new TFSResources(config);
 				List<String> subList = null;
-				if((iThreadCount-1) == i){
+				if((iThreadCount - 1) == i){
 					
-					subList = new ArrayList<String>(size - (i)*diffCount);
-					subList.addAll(pathList.subList((i)*diffCount, size - 1));
+					subList = new ArrayList<String>(size - (i) * diffCount);
+					subList.addAll(pathList.subList((i) * diffCount, size - 1));
+					TFSResourcesFactory.clearTFSManager(exec,latch,tfsr);
 				}else{
 					
 					subList = new ArrayList<String>(diffCount);
 					subList.addAll(pathList.subList((i)*diffCount, (i+1)*diffCount - 1));
 				}
 				tfsr.setResourcePathList(subList);
+				tfsr.setLatch(latch);
 				exec.execute(tfsr);
 			}
-			exec.shutdown();
+
+			//exec.shutdown();
 		}
+	}
+	
+	private static void clearTFSManager(final ExecutorService exec,final CountDownLatch latch,final TFSResources tfsr){
+		//启动清理线程
+		exec.execute(new Runnable(){
+
+			public void run() {
+				
+				try {
+					latch.await();
+					logger.info("所有任务线程均已完成，开始销毁TFS资源管理器。");
+					//
+					tfsr.destoryTFSResource();
+					exec.shutdownNow();
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+					logger.error(e.getMessage());
+				}
+				
+			}
+			
+		});
 	}
 }
