72f81d97777d3a9bcc015aabc256ccb55d99a5e1
Jeffrey Lee <leilitop@gmail.com>
2013-12-19 16:14:20 +0800

    日志输出相关改动 资源销毁相关改动

--- src/main/java/me/lilei/tfs/uploadfile/TFSResources.java
+++ src/main/java/me/lilei/tfs/uploadfile/TFSResources.java
@@ -2,6 +2,7 @@ package me.lilei.tfs.uploadfile;
 
 import java.util.List;
 import java.util.RandomAccess;
+import java.util.concurrent.CountDownLatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,11 @@ public class TFSResources implements Runnable{
 
 	private DefaultTfsManager tfsManager = null;
 	
+	private CountDownLatch latch = null;
+	public void setLatch(CountDownLatch latch) {
+		this.latch = latch;
+	}
+
 	//存储上传资源位置的列表
 	private List<String> resourcePathList = null;
 	
@@ -65,10 +71,12 @@ public class TFSResources implements Runnable{
 	}
 	
 	/**
-	 * TFS资源一旦不使用请销毁
+	 * TFS资源一旦不使用请销毁.
+	 * 多线程时候清理工作交给外部单独线程完成。
 	 */
 	public void destoryTFSResource() {
 		
+		//清理工作交给单独线程完成。
 		if(null != tfsManager){
 		
 			tfsManager.destroy();
@@ -81,36 +89,27 @@ public class TFSResources implements Runnable{
 			return;
 		}
 		long startTime=System.nanoTime();   //获取开始时间  
-		try {
-			//
-			if (pathList instanceof RandomAccess) {
+		//
+		if (pathList instanceof RandomAccess) {
+			
+			for(int i=0; i < pathList.size(); i++) {
 				
-				for(int i=0; i < pathList.size(); i++) {
-					
-					send1Resource2TFS(pathList.get(i));
-				}
-			} else {
-				
-				for (String strPath : pathList) {
-
-					send1Resource2TFS(strPath);
-				}
+				send1Resource2TFS(pathList.get(i));
 			}
+		} else {
+			
+			for (String strPath : pathList) {
 
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		} finally {
-
-			tfsManager.destroy();
+				send1Resource2TFS(strPath);
+			}
 		}
 		
 		long endTime=System.nanoTime(); //获取结束时间  
-        System.out.println("文件系统上传TFS耗时： " +(endTime-startTime)/timeTime+"[秒]");  
-        System.out.println("成功上传的文件数量： " + successCount +"[个]");  
-        System.out.println("上传失败的文件数量： " + failCount + "[个]");  
-        System.out.println("成功上传的文件总大小： " + totalFileSize + "[KB]");  
-        System.out.println("每秒上传大小： "+ totalFileSize /((endTime - startTime)/timeTime) + "[KB/s]");  
+        logger.info("文件系统上传TFS耗时： " +(endTime-startTime)/timeTime+"[秒]");  
+        logger.info("成功上传的文件数量： " + successCount +"[个]");  
+        logger.info("上传失败的文件数量： " + failCount + "[个]");  
+        logger.info("成功上传的文件总大小： " + totalFileSize + "[KB]");  
+        logger.info("每秒上传大小： "+ totalFileSize /((endTime - startTime)/timeTime) + "[KB/s]");  
 	}
 	
 	/**
@@ -124,7 +123,8 @@ public class TFSResources implements Runnable{
 		double fileSize = Long.parseLong(fileAttr[1]) / storgeTime;
 		String tfsSavedFileName = tfsManager.saveFile(fileAttr[0],
 				null, null);
-		System.out.println(strPath + "写入TFS耗时（单条）： "
+		logger.info(strPath + "--->" + tfsSavedFileName);
+		logger.debug(strPath + "写入TFS耗时（单条）： "
 				+ (System.nanoTime() - startTime_WTFS) / timeTime
 				+ "[秒]" + "SIZE=" + fileSize + "[KB]");
 		if (null == tfsSavedFileName) {
@@ -136,7 +136,7 @@ public class TFSResources implements Runnable{
 
 		long startTime_WF = System.nanoTime();
 
-		System.out.println("写入文件耗时（单条）： "
+		logger.debug("写入文件耗时（单条）： "
 				+ (System.nanoTime() - startTime_WF) / timeTime
 				+ "[秒]");
 	}
@@ -147,6 +147,9 @@ public class TFSResources implements Runnable{
 			return;
 		}
 		uploadResource2TFS(resourcePathList);
-		destoryTFSResource();
+		
+		if(null != latch){
+			latch.countDown();
+		}
 	}
 }
