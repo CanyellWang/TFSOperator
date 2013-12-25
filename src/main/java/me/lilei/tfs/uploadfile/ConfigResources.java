a2d4374c998fa6df3c4a951ab8c0f1a9408b265f
JeffreyLee <leilitop@gmail.com>
2013-12-25 22:24:51 +0800

    config加载方式与编码改进概述

--- src/main/java/me/lilei/tfs/uploadfile/ConfigResources.java
+++ src/main/java/me/lilei/tfs/uploadfile/ConfigResources.java
@@ -8,6 +8,9 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class ConfigResources {
 	
+	/**
+	 * 以静态方式首先加载日志框架的配置文件
+	 */
 	static{
 		//System.out.println(System.getProperty("user.dir"));
 		org.apache.log4j.PropertyConfigurator.configure(System.getProperty("user.dir") 
@@ -19,6 +22,15 @@ public class ConfigResources {
 	public static final String SPRING_APP_CONTEXT_PATH
 		= "file:" + System.getProperty("user.dir") + "/conf/root-context.xml";
 	
+	/**
+	 * TFS系统DataServer线程个数，用于控制本机程序向TFS写入文件的线程个数localWrite2TFSThreadCount，这个个数小于等于DSThreadCount。
+	 */
+	private int tfsDSThreadCount = 3;
+	
+	public int getTfsDSThreadCount() {
+		return tfsDSThreadCount;
+	}
+
 	public ConfigResources(){
 		
 		loadConfig();
@@ -29,12 +41,27 @@ public class ConfigResources {
 	 */
 	private void loadConfig(){
 		logger.debug(this.getClass().getResource("/").getPath());
+		loadSpringConfig();
+		loadTFSConfig();
+		logger.debug("Config loaded from file!");
+	}
+
+	/**
+	 * 利用spring加载相关的配置文件，比如DB设置，这些都是设置再root-context.xml中的
+	 */
+	private void loadSpringConfig(){
 		logger.debug(SPRING_APP_CONTEXT_PATH);
 		String[] locations = {SPRING_APP_CONTEXT_PATH};
 		//String[] locations = {"spring/appConfig/application-context.xml","spring/appConfig/application-mail.xml" };
 		@SuppressWarnings("unused")
 		ApplicationContext act = new FileSystemXmlApplicationContext(locations);
-		logger.debug("Config loaded from file!");
 	}
-
+	
+	/**
+	 * 处理TFS相关设置的配置文件
+	 */
+	private void loadTFSConfig(){
+		//TODO:从配置文件中或从远程服务器中读取TFS配置
+		tfsDSThreadCount = 4;
+	}
 }
--- src/main/java/me/lilei/tfs/uploadfile/TFSFileOpsLauncher.java
+++ src/main/java/me/lilei/tfs/uploadfile/TFSFileOpsLauncher.java
@@ -20,14 +20,20 @@ public class TFSFileOpsLauncher {
 	public static void main(String[] args) {
 		
 		
-		//1.初始化环境信息
+		//TODO:1.初始化环境信息
 		//EnvironmentInfo envInfo = EnvironmentInfoMaker.getCurrentEnvInfo();
 		
-		//2.启动heartbeat线程，执行heartbeat通信，传递EnvironmentInfo以及当前执行任务信息，同时获取服务器分配的任务信息
+		//TODO:2.启动heartbeat线程，执行heartbeat通信，传递EnvironmentInfo以及当前执行任务信息，同时获取服务器分配的任务信息
 		
-		//3.执行获取到的任务
+		//TODO:3.执行获取到的任务
 		
 	
+		//TODO:使用线程去启动LocalRes去扫描文件，设定一个阈值，或者是综合硬件性能计算出来一个阈值。向LIST添加文件到达这个阈值之后，为防止继续添加LIST将内存搞死，就挂起或是暂停扫描
+		//TODO：将达到阈值的LIST添加到队列（或是调用均衡方法，将LIST拆分成DS线程多个，然后放进队列），工作线程（<=DSCount）将队列内的LIST取出开始工作
+		//TODO:将完成传输的文件首先通过通用方法写到数据库中（其实是写本地同时写数据库，以防止数据库失败时候，程序跟着失败，但必须保证本地日志能够继续工作，远程写失败之后可以通过读取本地日志继续完成上传）
+		//TODO:写数据库的应该也丢到一个队列里，达到一个阈值之后dao才朝数据库中写，但是要注意阈值设定，放置这个队列和上边的队列一起把内存搞死
+		//TODO:工作线程全部完成时候通知loaclRes线程继续扫描，并重复以上步骤
+		
 		//本地上传文件位置取得
 		List<String> path = new ArrayList<String>(1);
 		path.add("/home/jeffrey/TFS-test");
